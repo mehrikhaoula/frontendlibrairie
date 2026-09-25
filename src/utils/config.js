80b83c331c6991a50144d59dbdac08588c9c14ca
mehrikhaoula <mehri.khawla@gmail.com>
2026-09-25 14:56:21 +0100

    Fix production API URL

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,11 @@
-const API_BASE_URL = "http://localhost:3010/api";
+const API_BASE_URL_BASE = "https://backlibrairie.onrender.com";
 
-export const endpoint = {
+const API_BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3010/api"
+    : `${API_BASE_URL_BASE}/api`;
+
+    export const endpoint = {
   login: `${API_BASE_URL}/login`,
 
   // categorie
